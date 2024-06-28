162d7b6fdccb2884166c697c7c14c0b71ebd3940
Egba Frank <Egbafrank72@gmail.com>
2024-06-28 21:39:48 +0000

    Added flask dependency

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 # Installs an older version of flask from pip3
 
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['werkzeug']
 }
