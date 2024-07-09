1154fc6bccd268817b95a7526232f63d16995c44
Egba Frank <Egbafrank72@gmail.com>
2024-07-09 13:11:11 +0000

    Added advanced task

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,39 @@
+# Install and configure an Nginx server
+
+exec { 'update':
+  path    => '/usr/bin/:',
+  command => 'apt update && apt -y upgrade',
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['update'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+file  { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+file_line { 'nginx_redirect':
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'location /redirect_me { return 301 https://www.alxafrica.com; }',
+  after   => 'server_name _;',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+file_line { 'custom_header':
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'add_header X-Served-By $hostname always;',
+  after   => 'try_files $uri $uri/ =404;',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
