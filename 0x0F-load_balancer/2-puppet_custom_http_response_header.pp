1e9c8453acf39c268f3ba452c77223830e636c59
Egba Frank <Egbafrank72@gmail.com>
2024-07-13 17:55:41 +0000

    Added changes globally

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -17,11 +17,10 @@ service { 'nginx':
 }
 
 file_line { 'custom_header':
-  path     => '/etc/nginx/sites-available/default',
-  line     => "\n\tadd_header X-Served-By ${hostname} always;",
-  after    => 'server_name _;',
-  require  => Package['nginx'],
-  multiple => false,
+  path    => '/etc/nginx/nginx.conf',
+  line    => "\n\tadd_header X-Served-By ${hostname} always;",
+  after   => 'http {',
+  require => Package['nginx'],
 }
 
 exec { 'restart_nginx':
