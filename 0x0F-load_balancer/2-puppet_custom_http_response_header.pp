2d3416484f0e7e29114727f380a884a904d4aac4
Egba Frank <Egbafrank72@gmail.com>
2024-07-13 12:58:42 +0000

    Updated puppet script

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -10,16 +10,37 @@ package { 'nginx':
   require => Exec['update'],
 }
 
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
+exec { 'configure_firewall':
+  command => 'ufw allow "Nginx HTTP"',
+  onlyif  => 'ufw --version',
+  path    => '/usr/bin/:',
   require => Package['nginx'],
 }
 
-file  { '/var/www/html/index.html':
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => [Exec['configure_firewall'], Package['nginx']],
+}
+
+file { '/var/www/html/index.html':
   ensure  => file,
   content => 'Hello World!',
   require => Package['nginx'],
+}
+
+file { '/var/www/html/404.html':
+  ensure  => file,
+  content => "Ceci n'est pas une page",
+  require => Package['nginx'],
+}
+
+file_line { 'nginx_custom_header':
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'add_header X-Served-By $hostname always;',
+  after   => 'listen 80 default_server;',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
 }
 
 file_line { 'nginx_redirect':
@@ -30,10 +51,32 @@ file_line { 'nginx_redirect':
   notify  => Service['nginx'],
 }
 
-file_line { 'custom_header':
+file_line { 'nginx_error_page':
   path    => '/etc/nginx/sites-available/default',
-  line    => 'add_header X-Served-By $hostname always;',
-  after   => '# as directory, then fall back to displaying a 404.',
+  line    => 'error_page 404 /404.html;',
+  after   => 'server_name _;',
   require => Package['nginx'],
   notify  => Service['nginx'],
 }
+
+file_line { 'nginx_404_location':
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'location = /404.html { internal; }',
+  after   => 'error_page 404 /404.html;',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+exec { 'reload_nginx':
+  command     => 'nginx -s reload',
+  refreshonly => true,
+  subscribe   => File_line['nginx_custom_header'],
+}
+
+exec { 'check_nginx_status':
+  command => 'curl -I http://localhost 2>/dev/null | grep -q "200 OK"',
+  unless  => 'curl -I http://localhost 2>/dev/null | grep -q "200 OK"',
+  path    => '/usr/bin/:',
+  require => Service['nginx'],
+}
+
