7387c0a021b9b338146fcd5bf69623de95c42b13
Egba Frank <Egbafrank72@gmail.com>
2024-08-14 13:45:08 +0000

    Changed file path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # Fixes a wordpress bug
 
 file_line { 'Fix-wordpress':
-  path  => '/home/vagrant/ALX/alx-system_engineering-devops/0x17-web_stack_debugging_3/wp-settings.php',
+  path  => '/var/www/html/wp-settings.php',
   line  => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );",
   match => '^require_once\( ABSPATH \. WPINC \. \'/class-wp-locale\.phpp\' \);$',
 }
